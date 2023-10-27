64a5ab2c7aa6bb940f9fb011e27d7bbe40f141f9
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-10-27 12:17:22 +0300

    ...

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
 {
-  const plnElement = document.querySelector(".js-pln");
-  const eurElement = document.querySelector(".js-eur");
-  const usdElement = document.querySelector(".js-usd");
   const formElement = document.querySelector(".js-form");
+  // const resultElement = document.querySelector(".js-result");
+
   const eursumElement = document.querySelector(".js-eursum");
   const usdsumElement = document.querySelector(".js-usdsum");
   const btcsumElement = document.querySelector(".js-btcsum");
 
+  const plnElement = document.querySelector(".js-pln");
+  const eurElement = document.querySelector(".js-eur");
+  const usdElement = document.querySelector(".js-usd");
+
   formElement.addEventListener("input", (event) => {
     event.preventDefault();
-
-    const eur = eurElement.value;
-    const usd = usdElement.value;
-    const pln = plnElement.value;
-    const btc = 119143;
-
-    const sum = pln * eur;
-    const sumUSD = pln * usd;
-    const sumBTC = pln / btc;
-
+  
+    let eur = eurElement.value;
+    let usd = usdElement.value;
+    let pln = plnElement.value;
+    let btc = 148143;
+  
+    let sum = pln * eur;
+    let sumUSD = pln * usd;
+    let sumBTC = pln / btc;
+  
     eursumElement.innerText = sum.toFixed(2);
     usdsumElement.innerText = sumUSD.toFixed(2);
     btcsumElement.innerText = sumBTC.toFixed(10);
   });
+  
 }
